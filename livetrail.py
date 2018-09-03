832b46d56b0f28ae64dbfdd5e58d9369ee7779b7
Jean-Paul Cornillon <jeanpaul.cornillon@gmail.com>
2018-09-03 14:20:21 +0200

    Adaptation url de Livetrail

--- livetrail.py
+++ livetrail.py
@@ -52,11 +52,14 @@ class Epreuve(object):
             self.nomEpreuve = lien.replace('http://livetrail.net/histo/','')
             self.type_lien=1
         elif lien.endswith('livetrail.net'):   
-            self.nomEpreuve = lien.replace('https://','').replace('.livetrail.net', '')
+            self.nomEpreuve = lien.replace('http://','').replace('.livetrail.net', '')
             self.type_lien=2
         elif lien.endswith('livetrail.run'):   
             self.nomEpreuve = lien.replace('http://','').replace('.livetrail.run', '')
             self.type_lien=2
+        elif lien.startswith('https://livetrail.net/live/'):   
+            self.nomEpreuve = lien.replace('https://livetrail.net/live/','')
+            self.type_lien=3
         else:
             print('Lien pourri !!!!')
             exit(1)
@@ -89,6 +92,8 @@ class Course(object):
         elif self.type_lien == 2:
             #self.lienCourse = 'http://{}.livetrail.net/classement.php'.format(self.nomCourse)
             self.lienCourse = 'http://{}.livetrail.run/classement.php'.format(self.nomCourse)
+        elif self.type_lien == 3:
+            self.lienCourse = 'https://livetrail.net/live/{}/classement.php'.format(self.nomCourse)
         else:
             print('Mauvais type de lien !!!!')
             exit(1)
@@ -98,6 +103,9 @@ class Course(object):
         data = urllib.parse.urlencode(datas)
         data = data.encode('utf-8')
         return data
+
+    def __str__(self):
+        return '{} - {} - {} - {} - {}'.format(self.nomCourse, self.course, self.lib, self.km_deniv, self.lienCourse)
 
 
 def make_dic_fic(fic):
@@ -119,7 +127,6 @@ def main(argv):
     for e in epreuve.courses:
         if e.course.lower() not in ['relais']:
             print(BOLD + '{} --> {} '.format(e.nomCourse, e.course) + RESET) 
-            #import pdb; pdb.set_trace()
             tools.fichiers = make_dic_fic('{}_{}'.format(e.nomCourse, e.course))
             tools.coureurs = tools.htmtoliste(e.lienCourse, e.data)
         
